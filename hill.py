34b0abd16672cc9a3065b187a0d690f7e0079b3a
yobosis <daksh.guzzar@gmail.com>
2026-05-14 22:34:23 +0530

    Update hill.py

--- hill.py
+++ hill.py
@@ -1,74 +1,24 @@
-# Convert letter ↔ number
-def to_num(c):
-    return ord(c) - ord('a')
+# Simple Hill Cipher
 
-def to_char(n):
-    return chr(n + ord('a'))
+key = [[3, 3], [2, 5]]
 
-
-# Encrypt
-def encrypt(text, key):
-    text = text.lower().replace(" ", "")
-    
-    # make even length
-    if len(text) % 2 != 0:
-        text += 'x'
-
+def hill(text):
+    text = text.lower()
     result = ""
 
     for i in range(0, len(text), 2):
-        a = to_num(text[i])
-        b = to_num(text[i+1])
+        a = ord(text[i]) - 97
+        b = ord(text[i+1]) - 97
 
-        # matrix multiplication
-        x = (key[0][0]*a + key[0][1]*b) % 26
-        y = (key[1][0]*a + key[1][1]*b) % 26
+        x = (3*a + 3*b) % 26
+        y = (2*a + 5*b) % 26
 
-        result += to_char(x) + to_char(y)
-
-    return result.upper()
-
-
-# Decrypt (using inverse matrix)
-def decrypt(text, key):
-    # determinant
-    det = key[0][0]*key[1][1] - key[0][1]*key[1][0]
-    det = det % 26
-
-    # find inverse of determinant (brute force)
-    for i in range(26):
-        if (det * i) % 26 == 1:
-            inv_det = i
-            break
-
-    # inverse matrix
-    inv = [
-        [ key[1][1]*inv_det % 26, -key[0][1]*inv_det % 26],
-        [-key[1][0]*inv_det % 26,  key[0][0]*inv_det % 26]
-    ]
-
-    result = ""
-
-    for i in range(0, len(text), 2):
-        a = to_num(text[i].lower())
-        b = to_num(text[i+1].lower())
-
-        x = (inv[0][0]*a + inv[0][1]*b) % 26
-        y = (inv[1][0]*a + inv[1][1]*b) % 26
-
-        result += to_char(x) + to_char(y)
+        result += chr(x + 65) + chr(y + 65)
 
     return result
 
-
-# Example
-key = [[3, 3],
-       [2, 5]]
-
 msg = "help"
 
-enc = encrypt(msg, key)
-dec = decrypt(enc, key)
+enc = hill(msg)
 
 print("Encrypted:", enc)
-print("Decrypted:", dec)
