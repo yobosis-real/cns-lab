1c17e3e64d24a5cec9d8e847596d5f9ea45a1f10
yobosis <daksh.guzzar@gmail.com>
2026-05-14 22:36:12 +0530

    Refactor Vigenere cipher implementation

--- vignere.py
+++ vignere.py
@@ -1,67 +1,29 @@
-# Convert letter ↔ number
-def to_num(c):
-    return ord(c) - ord('a')
+# Simple Vigenere Cipher
 
-def to_char(n):
-    return chr(n + ord('a'))
-
-
-# Generate repeated key
-def make_key(text, key):
+def cipher(text, key, mode):
+    result = ""
     key = key.lower()
-    result = ""
-    j = 0
 
-    for ch in text:
-        if ch.isalpha():
-            result += key[j % len(key)]
-            j += 1
-        else:
-            result += ch
+    for i in range(len(text)):
+        if text[i].isalpha():
+            t = ord(text[i].lower()) - 97
+            k = ord(key[i % len(key)]) - 97
+
+            if mode == "e":
+                x = (t + k) % 26
+            else:
+                x = (t - k) % 26
+
+            result += chr(x + 65)
 
     return result
 
 
-# Encrypt
-def encrypt(text, key):
-    text = text.lower()
-    key = make_key(text, key)
-
-    result = ""
-
-    for i in range(len(text)):
-        if text[i].isalpha():
-            x = (to_num(text[i]) + to_num(key[i])) % 26
-            result += to_char(x)
-        else:
-            result += text[i]
-
-    return result.upper()
-
-
-# Decrypt
-def decrypt(text, key):
-    text = text.lower()
-    key = make_key(text, key)
-
-    result = ""
-
-    for i in range(len(text)):
-        if text[i].isalpha():
-            x = (to_num(text[i]) - to_num(key[i])) % 26
-            result += to_char(x)
-        else:
-            result += text[i]
-
-    return result
-
-
-# Example
-msg = "HELLO WORLD"
+msg = "HELLO"
 key = "KEY"
 
-enc = encrypt(msg, key)
-dec = decrypt(enc, key)
+enc = cipher(msg, key, "e")
+dec = cipher(enc, key, "d")
 
 print("Encrypted:", enc)
 print("Decrypted:", dec)
